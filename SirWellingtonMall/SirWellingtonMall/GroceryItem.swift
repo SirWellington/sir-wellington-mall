2fd0174b63cb438bfcc38e083348ed46a1bf89b0
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-05-14 10:59:37 -0700

    Adding Grocery Item Struct

---
+++ SirWellingtonMall/SirWellingtonMall/GroceryItem.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryItem.swift
+//  SirWellingtonMall
+//
+//  Created by Juan Wellington Moreno on 5/14/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct GroceryItem {
+    
+    var name: String? = nil
+    var description: String? = nil
+    var image: UIImage? = nil
+}
